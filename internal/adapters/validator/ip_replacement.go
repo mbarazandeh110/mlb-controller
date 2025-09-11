131774c44a4141f9740992c203ca88d82f0d365d
mbarazandeh110 <m.barazandeh110@gmail.com>
2025-09-11 19:49:55 +0330

    check network overlap in ip_replicement.go

--- internal/adapters/validator/ip_replacement.go
+++ internal/adapters/validator/ip_replacement.go
@@ -32,20 +32,22 @@ func (v *GlobalIPReplacementValidator) Validate(cfg *config.Config) error {
 	}
 
 	// Validate nets
-	sourceNets := make(map[string]struct{})
 	for _, net := range cfg.GlobalIPReplacementList.Net {
-		for _, n := range net.Nets {
+		for i, n := range net.Nets {
 			if !util.IsValidIP(n.Source) || !util.IsValidIP(n.Target) {
 				return fmt.Errorf("invalid IP in global_ip_replacement_list.net: source=%s, target=%s", n.Source, n.Target)
 			}
 			if n.Mask < 0 || n.Mask > 32 {
 				return fmt.Errorf("global_ip_replacement_list.net.mask must be between 0 and 32")
 			}
-			sourceNet := fmt.Sprintf("%s/%d", n.Source, n.Mask)
-			if _, exists := sourceNets[sourceNet]; exists {
-				return fmt.Errorf("global_ip_replacement_list.net source '%s' must not overlap", sourceNet)
+			for j, n2 := range net.Nets {
+				if i == j {
+					continue
+				}
+				if util.IsNetworkOverlap(n.Source, n.Mask, n2.Source, n2.Mask) {
+					return fmt.Errorf("global_ip_replacement_list.net source '%s/%d' and '%s/%d' must not overlap", n.Source, n.Mask, n2.Source, n2.Mask)
+				}
 			}
-			sourceNets[sourceNet] = struct{}{}
 		}
 	}
 
