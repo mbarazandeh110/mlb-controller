45469857df1eba062de68f1b6c1908d1ee47516c
mbarazandeh110 <m.barazandeh110@gmail.com>
2025-09-16 04:03:20 +0330

    correct ListBackends output

--- internal/ports/loadbalancer/loadbalancer.go
+++ internal/ports/loadbalancer/loadbalancer.go
@@ -9,7 +9,7 @@ import (
 // LoadBalancerAdapter defines the interface for interacting with a load balancer (e.g., NGINX, Envoy).
 type LoadBalancerAdapter interface {
 	// ListBackends retrieves the current backends for a given upstream.
-	ListBackends(ctx context.Context, upstreamName string) ([]model.Backend, error)
+	ListBackends(ctx context.Context, upstreamName string) (map[string][]model.Backend, error)
 	// AddBackend adds a new backend to the specified upstream.
 	AddBackend(ctx context.Context, upstreamName string, backend model.Backend) error
 	// RemoveBackend removes a backend from the specified upstream.
